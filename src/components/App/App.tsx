a92f5350184520e39d695470dcee7a4c10479052
duynhanf <duynhanf@gmail.com>
2020-07-11 12:18:47 +0900

    Creating the Event List Component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Recorder from "../Recorder";
+import Calendar from "../Calendar";
 
 function App() {
-  return <div>
-    <Recorder />
-  </div>;
+  return (
+    <div>
+      <Recorder />
+      <Calendar />
+    </div>
+  );
 }
 
 export default App;
